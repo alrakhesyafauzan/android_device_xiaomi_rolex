58205b6b4cfffca23c01d27d0090424c06eda465
alrakhesyafauzan <alrakhesyafauzan@gmail.com>
2018-12-24 19:38:00 +0700

    rolex : Pixel Experience BringUp
    Signed-off-by: alrakhesyafauzan <alrakhesyafauzan@gmail.com>

--- aosp_rolex.mk
+++ aosp_rolex.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from rolex device
 $(call inherit-product, device/xiaomi/rolex/device.mk)
@@ -29,7 +29,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := rolex
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 4A
-PRODUCT_NAME := lineage_rolex
+PRODUCT_NAME := aosp_rolex
+TARGET_GAPPS_ARCH := arm64
+TARGET_DENSITY := xhdpi
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
