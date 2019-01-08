afcef06961e0560acfb964eb2e35695a1f4bb629
alrakhesyafauzan <alrakhesyafauzan@gmail.com>
2019-01-08 17:00:46 +0700

    rolex: Resolve vendor mismatch issue
    * Remove those marks sets this separate to PRODUCT_BUILD_PROP_OVERIDES
      making vendor mismatch to be fixed
    
    Signed-off-by: alrakhesyafauzan <alrakhesyafauzan@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -45,4 +45,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys"
+BUILD_FINGERPRINT := Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys
